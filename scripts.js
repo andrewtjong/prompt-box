6ec307453b0137e35c09f1afa1eed35eeed0c8a8
Andrew Tjong <72036357+andrewtjong@users.noreply.github.com>
2021-10-07 15:19:33 +0200

    add popup into the DOM

--- scripts.js
+++ scripts.js
@@ -3,21 +3,26 @@ function ask(options) {
     // First, need to create a popup with all the fields in it
     const popup = document.createElement('form');
     popup.classList.add('popup');
-    popup.insertAdjacentHTML('afterbegin', `
+    popup.insertAdjacentHTML(
+      'afterbegin', 
+      `
     <fieldset>
-    <label>${options.title}</label>
+      <label>${options.title}</label>
     </fieldset>
     `);
     
     // Second, check if they want a cancel button
     if(options.cancel) {
-      const skiButton = document.createElement('button');
-      skiButton.type = 'button';
-      skiButton.textContent = 'Cancel';
+      const skipButton = document.createElement('button');
+      skipButton.type = 'button';
+      skipButton.textContent = 'Cancel';s
       // TODO: listen for a click on that cancel button
-    }
+    };
 
     // Third, listen for the submit event on the inouts
     // Fourth, when someone does submit it resolve the data that was in the input box
-  })
+
+    // insert that popup into the DOM
+    document.body.appendChild(popup);
+  });
 }
