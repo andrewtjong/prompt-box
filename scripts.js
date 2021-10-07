9297988835645c6ff7b52bb1fe1ede2473adbf45
Andrew Tjong <72036357+andrewtjong@users.noreply.github.com>
2021-10-07 13:59:44 +0200

    Create form and add class of .popup

--- scripts.js
+++ scripts.js
@@ -1,8 +1,11 @@
 function ask(options) {
   return new Promise(function(resolve) {
-    //First, need to create a popup with all the fields in it
+    // First, need to create a popup with all the fields in it
+    const popup = document.createElement('form');
+    popup.classList.add('popup')
+
     // Second, check if they want a cancel button
     // Third, listen for the submit event on the inouts
-    // Fourth, when someone does submit itresolve the data that was in the input box
+    // Fourth, when someone does submit it resolve the data that was in the input box
   })
 }
