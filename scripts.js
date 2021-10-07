2421df15ad2667a199e1187b502667641c265e6c
Andrew Tjong <72036357+andrewtjong@users.noreply.github.com>
2021-10-07 14:13:39 +0200

    add html fieldset and label

--- scripts.js
+++ scripts.js
@@ -2,8 +2,13 @@ function ask(options) {
   return new Promise(function(resolve) {
     // First, need to create a popup with all the fields in it
     const popup = document.createElement('form');
-    popup.classList.add('popup')
-
+    popup.classList.add('popup');
+    popup.insertAdjacentHTML('afterbegin', `
+    <fieldset>
+    <label>${options.title}</label>
+    </fieldset>
+    `);
+    console.log(popup);
     // Second, check if they want a cancel button
     // Third, listen for the submit event on the inouts
     // Fourth, when someone does submit it resolve the data that was in the input box
