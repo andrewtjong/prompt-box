891cf4e800e58eec34bc34d1587a6f231cd3789e
Andrew Tjong <72036357+andrewtjong@users.noreply.github.com>
2021-10-07 13:57:06 +0200

    Write and set out features that need to be accomplished

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log('is it working')
+function ask(options) {
+  return new Promise(function(resolve) {
+    //First, need to create a popup with all the fields in it
+    // Second, check if they want a cancel button
+    // Third, listen for the submit event on the inouts
+    // Fourth, when someone does submit itresolve the data that was in the input box
+  })
+}
